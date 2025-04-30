8b82b0bf98e1eb70fc7ad7777a2f32ffcc4965ba
RomanVintoniak <romanvintoniak03@gmail.com>
2025-04-30 10:39:42 +0300

    Add Class Library and Web API projects

---
+++ CarInsuranceSales.Api/Controllers/CarInsuranceSales.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarInsuranceSales.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarInsuranceSales : ControllerBase
+    {
+
+    }
+}
